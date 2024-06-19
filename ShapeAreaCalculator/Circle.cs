54963ec8c01e7222ab4eae88ffc939b2860d3f4a
MindiashviliRoman <mindiashvilirr@gmail.com>
2024-06-19 16:09:16 +0300

    Initial commit

---
+++ ShapeAreaCalculator/Circle.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ShapeAreaCalculator
+{
+    public class Circle : IShapeArea
+    {
+        private float _radius;
+
+        public float Radius => _radius;
+
+        public Circle(float radius) 
+        {
+            _radius = radius;
+        }
+
+        public float Area()
+        {
+            return 3.14f * _radius * _radius;
+        }
+    }
+}
